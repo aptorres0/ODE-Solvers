29d0553b15bb195ec0c02063e3681bd8156cc463
aptorres0 <apault1@lsu.edu>
2024-12-22 23:02:55 -0400

    Update the centered difference boundary value problem code.

--- ODEsolvers/centered_PaulTorres.m
+++ ODEsolvers/centered_PaulTorres.m
@@ -1,46 +1,30 @@
 function [y,x] = centered_PaulTorres(P,Q,R,a,b,alpha,beta,N)
 %--------------------------------------------------------------------------
-%This fuction returns the solution of the equation y''+P(x)y'+Q(x)y = R(x)
-%with boundary values y(a) = alpha and y(b) = beta on the interval [a,b]
-%using the Centered Difference scheme on a uniform mesh of N grid points.
+% This fuction returns the solution of the equation y''+P(x)y'+Q(x)y = R(x)
+% with boundary values y(a) = alpha and y(b) = beta on the interval [a,b]
+% using the Centered Difference scheme on a uniform mesh of N+1 grid points.
 %--------------------------------------------------------------------------
-%Functions P,Q,& R are function handles to an arbitrary functions.
+% Functions P,Q,& R are function handles to an arbitrary functions.
 %--------------------------------------------------------------------------
 
 x = linspace(a,b,N+1); % N sub-intervals, N+1 points
 y = zeros(N+1,1); % pre-allocate solution (this is more efficient for MATLAB)
-y(1) = alpha; % initial condition
+y(1) = alpha; % 1st Boundary Condition
 y(N+1) = beta; % 2nd Boundary Condition
 h = (b-a)/N; % size of uniform mesh
 
-L = zeros(N-1,N-1); % pre-allocate linear transformation matrix
-L(1,1) = -(2-h*h*Q(x(2)));
-L(1,2) = 1+.5*h*P(x(2));
-L(N-1,N-2) = 1-.5*h*P(x(N));
-L(N-1,N-1) = -(2-h*h*Q(x(N)));
+% For the internal points, it is possible to write the finite difference
+% equations as Ly=b where L is a tridiagonal matrix
+% if there are N+1 points in our mesh, then there are N-1 internal grid 
+% for which we need to solve for.
+L = diag(-(2-h*h*Q(x(2:N)))); % Populates the main diagonal
+L = L + diag(1+0.5*h*P(x(2:N-1)),1); % Populates the right diagonal
+L = L + diag(1-0.5*h*P(x(3:N)),-1); % Populates the left diagonal
 
-b = zeros(N-1,1);
-b(1,1) = h*h*R(x(2))-(1-.5*h*P(x(2)))*alpha;
-b(N-1,1) = h*h*R(x(N))-(1+.5*h*P(x(N)))*beta;
+b = h*h*R(x(2:N));
+b(1) = b(1) - (1-.5*h*P(x(2)))*alpha;
+b(end) = b(end) - (1+.5*h*P(x(N)))*beta;
+b = b';
 
-for i = 2:N-2
-    L(i,i) = -(2-h*h*Q(x(i+1)));
-    L(i,i+1) = 1+.5*h*P(x(i+1));
-    L(i,i-1) = 1-.5*h*P(x(i+1));
-    
-    b(i,1) = h*h*R(x(i+1));
-    
-end
-
-
-y_unknown = L\b; %Solve for all values of y between a & b
-
-for i = 2:N
-    y(i,1) = y_unknown(i-1,1);
-end
-
-
-
-
-    
-
+y_h = L\b;
+y(2:N) = y_h;
